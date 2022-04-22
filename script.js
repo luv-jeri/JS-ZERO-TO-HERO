f717cb220b037656fe0c79270dbd14d3dada9782
luv-jeri <luvvjeri@gmail.com>
2022-04-22 20:29:51 +0530

    Day 1

---
+++ script.js
@@ -0,0 +1,16 @@
+// var num = 23934;
+// var num2 = 2089850;
+
+// var result = num - num2; //` 100 + 200
+
+// var Name = prompt('Whats you Name?');
+// var age = prompt('Whats you age?');
+
+
+// alert(093476);
+// var res = prompt();
+
+
+const myVar = 100
+
+console.log(myVar);
